64d16e682af88e3207a29d7b9d6df3b080b3aa38
JakeVanCleave <jake@jake>
2023-03-28 15:00:07 -0700

    added a console API and worked with CLI a bit

--- lib/cli.py
+++ lib/cli.py
@@ -1,2 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from rich.console import Console
+
+console = Console(width=80)
+
+engine = create_engine('sqlite:///restaurants.db')
+session = sessionmaker(bind=engine)()
+
 if __name__ == '__main__':
-    user_input = input("Please input something: ")
+    console.print('''[dark_red]
+  ___   _  _   _____  _            ___  ___
+ / _ \ | || | |_   _|| |           |  \/  |                         
+/ /_\ \| || |   | |  | |__    ___  | .  . |  ___  _ __   _   _  ___ 
+|  _  || || |   | |  | '_ \  / _ \ | |\/| | / _ \| '_ \ | | | |/ __|
+| | | || || |   | |  | | | ||  __/ | |  | ||  __/| | | || |_| |\__ \    
+\_| |_/|_||_|   \_/  |_| |_| \___| \_|  |_/ \___||_| |_| \__,_||___/
+
+
+                    d888P
+      d8b d8888P:::P
+    d:::888b::::::P
+   d:::dP8888b:d8P
+  d:::dP 88b  Yb   .d8888b.
+ d::::P  88Yb  Yb .P::::::Y8b
+ 8:::8   88`Yb  YbP::::   :::b
+ 8:::P   88 `8   8!:::::::::::b
+ 8:dP    88  Yb d!!!::::::::::8
+ 8P    ..88   Yb8!!!::::::::::P
+  .d8:::::Yb  d888VKb:!:!::!:8
+ d::::::  ::dP:::::::::b!!!!8
+8!!::::::::P::::::::::::b!8P
+8:!!::::::d::::::: ::::::b
+8:!:::::::8!:::::::  ::::8
+8:!!!:::::8!:::::::::::::8
+Yb:!!:::::8!!::::::::::::8
+ 8b:!!!:!!8!!!:!:::::!!:dP
+  `8b:!!!:Yb!!!!:::::!d88
+ VK   """  Y88!!!!!!!d8P                   
+    ''', justify="default")
+
+    console.print("[bold magenta]Heres a list of restaurants to order from: ")
+
+    user_input = console.input("[bold yellow]Please enter which restaurant you'd like to order from: ")
