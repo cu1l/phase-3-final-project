1ec77ed07fa291d905e18aaefcdfead49a73afe4
JakeVanCleave <jake@jake>
2023-03-28 13:16:25 -0700

    set up some CLI test stuff, did some models for db

---
+++ lib/db/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy import PrimaryKeyConstraint, ForeignKey, Table, Column, Integer, Float, String
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+
+
+engine = create_engine('sqlite:///restaurants.db')
+
+Base = declarative_base()
+
+restraunt_menus = Table(
+    'restaurants_menus',
+    Base.metadata,
+    Column('menu_id', ForeignKey('menu_items.id'), primary_key=True),
+    Column('restaurant_id', ForeignKey('restaurants.id'), primary_key=True)
+)
+
+class MenuItem(Base):
+    __tablename__ = 'menu_items'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+
+    id = Column(Integer())
+    food_name = Column(String())
+    food_price = Column(Integer())
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+
+    id = Column(Integer())
+    name = Column(String())
+    rating = Column(Integer())
